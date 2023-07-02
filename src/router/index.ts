51d9b3fef3ed0916af6e1d4413c1b9b3e7be6359
S-Traut <stephane.traut@gmail.com>
2023-07-02 21:20:55 +0200

    Update image & fix scroll

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,9 @@ import ContactView from '../views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior() {
+    return { top: 0 };
+  },
   routes: [
     {
       path: '/',
